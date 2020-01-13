5cb52e4e6be3d7ee2077cbda3f0f2dc4ba9b760d
Никитка <nikita79134751837@mail.ru>
2020-01-13 18:28:42 +0700

    отредактированы входные данные

--- function.py
+++ function.py
@@ -1,14 +1,15 @@
 #Распределяем на каждую локацию по машине
-def OneCarOneLocation(N, S, t):
+def OneCarOneLocation(N, KA, S, t, wells):
     # копия переменной задачи Х, только кол-вo машин = число локаций
-    x = [[[0 for k in range(N)] for j in range(N)] for i in range(N)]
-    y = [[0 for k in range(N)] for i in range(N)]  # посещает или нет ТС с номером К объект i
+    x = [[[0 for k in range(KA)] for j in range(N)] for i in range(N)]
+    y = [[0 for k in range(KA)] for i in range(N)]  # посещает или нет ТС с номером К объект i
     for k in range(N):
         y[0][k] = 1
-    s = [[0 for k in range(N)] for i in range(N)]  # время работы ТС номером К на объекте i
-    a = [[0 for k in range(N)] for i in range(N)]  # время прибытия ТС с номером К на объект i
+    s = [[0 for k in range(KA)] for i in range(N)]  # время работы ТС номером К на объекте i
+    a = [[0 for k in range(KA)] for i in range(N)]  # время прибытия ТС с номером К на объект i
 
     for j in range(1, N):
+        if (wells[j] > 1)
         x[0][j][j] = 1
         y[j][j] = 1
         s[j][j] = S[j]
