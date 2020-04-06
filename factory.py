5f6dc0b14e094055b10bd7ebc45a8294bcf46d25
Попов Никита <ni.popov@cft.ru>
2020-04-07 01:11:34 +0700

    CrossOper. Добавлено в фактори чтение из файла входных данных

--- factory.py
+++ factory.py
@@ -1,8 +1,7 @@
 from math import sqrt
+import csv
 
-N = 10  # число объектов
-K = 5  # набор всех ТС
-
+v = 50  # скорость ТС
 car_cost = 10000  # цена за арнеду машины
 penalty = 1000  # штраф за превышения временного срока
 
@@ -12,6 +11,9 @@ param_population = 5  # параметр который будет показы�
 param_min_num_cl_in_car = 2  # параметр отвечающий за, сколько минимально клиентов может присутствовать в однрой машине
 param_crossing = param_population * 2  # сколько раз будем скрещивать решения
 param_local_search = 10  # параметр для локального поиска, сколько раз будем переставлять клиента
+
+N = 10  # число объектов
+K = 5  # набор всех ТС
 
 OX = [0, -27, 21, 50, 31, -25, 31, -43, -35, -14]  # координаты по ОХ
 OY = [0, -44, 38, 42, 30, 39, 15, -23, 28, 20]  # координаты по ОУ
@@ -40,3 +42,94 @@ e = [0, 7, 4, 8, 9, 7, 8, 3, 9, 8]  # начало работы на i объе�
 l = [0 for j in range(N)]  # конец работы на i объекте
 for i in range(N):
     l[i] = e[i] + S[i]
+
+
+# Создаем пустые списки для входных данных
+# OX = []
+# OY = []
+# S = []
+# wells = []
+# e = []
+# l = []
+# N = 0
+# K = 0
+#
+# # Читаем файл с клиентами
+# FILENAME1 = "input/25/customers1.csv"
+#
+# with open(FILENAME1) as File:
+#     reader = csv.reader(File, delimiter=';', quotechar=';',
+#                         quoting=csv.QUOTE_MINIMAL)
+#     for row in reader:
+#         if row[0] != 'Name':
+#             OX.append(float(row[1]))
+#             OY.append(float(row[2]))
+#             S.append(float(row[3]))
+#             wells.append(float(row[4]))
+#             e.append(float(row[5]))
+#             l.append(float(row[6]))
+#             N += 1
+#
+# # Читаем файл с машинками, из него берем координаты для депо и добавляем в конец,
+# # для депо ставим 0 работ , 0 скважин и нулевое время
+# FILENAME2 = "input/25/vehicles1.csv"
+#
+# with open(FILENAME2) as File:
+#     reader = csv.reader(File, delimiter=';', quotechar=';',
+#                         quoting=csv.QUOTE_MINIMAL)
+#     for row in reader:
+#         if row[0] == 'Vehicle 1':
+#             OX.append(float(row[1]))
+#             OY.append(float(row[2]))
+#             S.append(0)
+#             wells.append(0)
+#             e.append(0)
+#             l.append(0)
+#             N += 1
+#
+#         # Считаем кол-во машин
+#         if row[0] != 'Name':
+#             K += 1
+#
+# KA = 0  # кол-во ТС = кол-ву скважин
+# for i in range(N):
+#     KA += wells[i]
+#
+# # Теперь разворачиваем решение чтобы депо было в начале
+# OX.reverse()
+# OY.reverse()
+# S.reverse()
+# wells.reverse()
+# e.reverse()
+# l.reverse()
+#
+# # Строим матрицу расстояний и времени
+# d = [[0 for j in range(N)] for i in range(N)]  # расстояния между городами
+# for i in range(N):
+#     for j in range(N):
+#         # d[i][j] = 111.1 * acos(sin(OX[i]) * sin(OX[j]) + cos(OX[i]) * cos(OX[j]) * cos(OY[j] - OY[i]))
+#         d[i][j] = sqrt(pow((OX[i] - OX[j]), 2) + pow((OY[i] - OY[j]), 2))
+#
+# t = [[0 for j in range(N)] for i in range(N)]  # время перемещения между городами
+# for i in range(N):
+#     for j in range(N):
+#         t[i][j] = (d[i][j] * 40) / (v * 24)
+#
+#
+# print("d = ")
+# for i in range(N):
+#     for j in range(N):
+#         print(d[i][j], end=' ')
+#     print('\n')
+# print('\n')
+# print("t = ")
+# for i in range(N):
+#     for j in range(N):
+#         print(t[i][j], end=' ')
+#     print('\n')
+# print('\n')
+#
+# for i in range(N):
+#     print(OX[i], " ", OY[i], " ", S[i], " ", wells[i], " ", e[i], " ", l[i])
+# print(K)
+# print(KA)
